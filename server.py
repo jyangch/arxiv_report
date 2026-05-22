ad063ff65458e17c1343a18439581b813406eb9d
Jun Yang <jyang.astro@gmail.com>
2026-05-22 11:10:44 +0800

    feat: scaffold FastAPI app, test fixtures, and requirements

---
+++ server.py
@@ -0,0 +1,11 @@
+"""FastAPI web UI for the arxiv_report daily report generator."""
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from arxiv_report.render import REPORTS_DIR
+
+app = FastAPI(title='arXiv astro-ph.HE Daily Report')
+app.mount('/static', StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory='templates')
